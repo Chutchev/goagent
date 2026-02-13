5abe6c3109f087033069c5704c600ce130e11697
i.chutchev <i.chutchev@mts.ai>
2026-02-13 21:06:24 +0300

    сделал стандартного агента v2

--- pkg/agent/Agent.go
+++ pkg/agent/Agent.go
@@ -1,114 +1,42 @@
 package agent
 
 import (
-	"bufio"
-	"context"
-	"fmt"
-	"github.com/Chutchev/goagent/pkg/clients/llm"
-	"github.com/Chutchev/goagent/pkg/config"
 	"log"
-	"log/slog"
-	"os"
-	"os/signal"
-	"strings"
-	"syscall"
 )
 
 type Agent struct {
 	systemPrompt string
-	Name         string
-	Mode         string
+	userPrompt   string
+	name         string
+	mode         string
 }
 
-func NewAgent(promptFile *string, name, mode string) *Agent {
-	systemPrompt, err := os.ReadFile(*promptFile)
-	if err != nil {
-		log.Fatalf("system prompt file read failed: %v", err)
-	}
+type AgentInterface interface {
+	RunInteractive()
+	RunGRPC()
+	RunHTTP()
+	Do(string)
+	GetSystemPrompt() string
+	GetUserPrompt() string
+	GetMode() string
+	GetName() string
+}
+
+func NewAgent(systemPrompt, userPrompt, name, mode string) AgentInterface {
+
 	return &Agent{
-		systemPrompt: string(systemPrompt),
-		Name:         name,
-		Mode:         mode,
+		systemPrompt: systemPrompt,
+		userPrompt:   userPrompt,
+		name:         name,
+		mode:         mode,
 	}
 }
 
-func (a *Agent) runInteractive() {
-	var lines []string
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Println("Введите текст (пустая строка для завершения):")
-		for {
-			fmt.Print("> ")
-			scanner.Scan()
-			text := scanner.Text()
-
-			// Если строка пустая - завершаем ввод
-			if text == "" {
-				break
-			}
-
-			lines = append(lines, text)
-		}
-		prompt := strings.Join(lines, "\n")
-		a.do(prompt)
-	}
-}
-
-func (a *Agent) runGRPC() {
-}
-
-func (a *Agent) runHTTP() {
-
-}
-
-func (a *Agent) survey() {}
-
-func (a *Agent) Run() {
-	switch a.Mode {
-	case "i":
-		go a.runInteractive()
-	case "grpc":
-		go a.runGRPC()
-	case "http":
-		go a.runHTTP()
-	default:
-		log.Fatal("")
-	}
-
-	// Сюда вставить запуск HTTP Сервера
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-	<-quit
-	slog.Info("Получен сигнал завершения, останавливаемся...")
-}
-
-func (a *Agent) do(userPrompt string) {
-	cfg := config.GetConfig()
-
-	c := llm.NewLLMClient(
-		cfg.LLMBaseURL,
-		cfg.LLMConfig.LLMToken,
-	)
-
-	req := llm.ChatRequest{
-		Model:       cfg.LLMConfig.LLMModel,
-		Temperature: cfg.LLMConfig.Temperature,
-		TopP:        cfg.LLMConfig.TopP,
-		Seed:        cfg.LLMConfig.Seed,
-		Messages: []llm.Message{
-			{
-				Role:    "system",
-				Content: a.systemPrompt,
-			},
-			{
-				Role:    "user",
-				Content: userPrompt,
-			},
-		},
-	}
-	r, err := c.CreateChatCompletion(context.Background(), req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(r.Choices[0].Message.Content)
-}
+func (a *Agent) RunInteractive()         { log.Fatal("Agent interactive not implemented.") }
+func (a *Agent) RunGRPC()                { log.Fatal("Agent grpc not implemented.") }
+func (a *Agent) RunHTTP()                { log.Fatal("Agent http not implemented.") }
+func (a *Agent) GetSystemPrompt() string { return a.systemPrompt }
+func (a *Agent) GetUserPrompt() string   { return a.userPrompt }
+func (a *Agent) Do(s string)             { log.Print("Agent Do not implemented") }
+func (a *Agent) GetMode() string         { return a.mode }
+func (a *Agent) GetName() string         { return a.name }
